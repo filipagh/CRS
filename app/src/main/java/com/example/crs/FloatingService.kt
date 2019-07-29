febd5b347945b5b6528feb44aec980e741df66c9
Pumec <Filip11agh@gmail.com>
2019-07-29 02:40:22 +0200

    code refactor add text parser

--- app/src/main/java/com/example/crs/FloatingService.kt
+++ app/src/main/java/com/example/crs/FloatingService.kt
@@ -1,34 +1,21 @@
 package com.example.crs
 
-import android.app.Service
-import android.content.Context
 import android.content.Intent
-import android.graphics.*
-import android.hardware.display.DisplayManager
-import android.media.Image
+import android.graphics.Color
 import android.media.ImageReader
-import android.media.projection.MediaProjection
-import android.media.projection.MediaProjectionManager
 import android.os.IBinder
-import android.provider.Settings
-import android.service.media.MediaBrowserService
-import android.support.v7.app.AppCompatActivity
-import android.view.*
-import com.bsk.floatingbubblelib.FloatingBubbleService
-import com.bsk.floatingbubblelib.FloatingBubbleConfig
-import kotlinx.android.synthetic.main.floating_service_layout.view.*
-import android.graphics.Bitmap.CompressFormat
-import android.graphics.Bitmap
+import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
-import com.bsk.floatingbubblelib.FloatingBubbleTouchListener
-import java.io.FileOutputStream
-import java.io.IOException
+import com.bsk.floatingbubblelib.FloatingBubbleConfig
+import com.bsk.floatingbubblelib.FloatingBubbleService
 
 class FloatingService : FloatingBubbleService() {
 
     lateinit var intentMain: Intent
-    var mediaProjection: MediaProjection? = null
+//    var mediaProjection: MediaProjection? = null
+    var mediaProjectionUtil: MediaProjectionUtil? = null
     lateinit var mImageReader: ImageReader
     lateinit var root: View
 
@@ -72,84 +59,45 @@ class FloatingService : FloatingBubbleService() {
 
             // Building
             .build()
+
     }
 
 
     fun setup(vstup: View) {
-
-        if (mediaProjection == null) {
-            val resultCode = intentMain!!.getIntExtra("resultCode", 0)
-            val data = intentMain.getParcelableExtra<Intent>("mediaProjectionData")
-            val mediaProjectManag =
-                getSystemService(AppCompatActivity.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-            mediaProjection = mediaProjectManag.getMediaProjection(resultCode, data)
+        if (mediaProjectionUtil == null) {
+            mediaProjectionUtil = MediaProjectionUtil()
+            mediaProjectionUtil!!.setupMediaProjection(intentMain,this)
         }
-
-        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        var windowSize = Point()
-        windowManager.defaultDisplay.getSize(windowSize)
-
-        mImageReader = ImageReader.newInstance(windowSize.y, windowSize.y, PixelFormat.RGBA_8888, 20)
-
-        val display = mediaProjection?.createVirtualDisplay(
-            "ScreenCapture",
-            windowSize.x, windowSize.y, 1000,
-            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-            mImageReader.surface, null, null
-        )
-
-        var btn = root.findViewById(R.id.scrShot) as Button
+        val btn = root.findViewById(R.id.scrShot) as Button
         btn.isEnabled = true
-
-
     }
 
     fun scrShot(view: View) {
-
-        var btn = root.findViewById(R.id.scrShot) as Button
+        val btn = root.findViewById(R.id.scrShot) as Button
         btn.isEnabled = false
         setState(false)
-        var image: Image? = null
-        var bitmap: Bitmap? = null
-        var i = 0
-        while (i != 100000) {
-            i++
-        }
-        val surf = root.findViewById(R.id.imageView) as ImageView
-        image = mImageReader.acquireNextImage()
-        val planes = image!!.planes
 
-        val buffer = planes[0].buffer.rewind()
-        bitmap = Bitmap.createBitmap(windowSize.y, windowSize.y, Bitmap.Config.ARGB_8888)
-        bitmap!!.copyPixelsFromBuffer(buffer)
-
-        val a = Bitmap.createBitmap(bitmap, 0, 0, windowSize.x, windowSize.y)
-
-
-        val c = Canvas(a)
+        val scrShot = mediaProjectionUtil!!.makeScrShot()
+        mediaProjectionUtil!!.parseTextFromBitmap(scrShot)
         setState(true)
-        surf.setImageBitmap(a)
-        var x = 4
+        val surf = root.findViewById(R.id.imageView) as ImageView
+        surf.setImageBitmap(scrShot)
 
-        var btnClear = root.findViewById(R.id.button2) as Button
+        val btnClear = root.findViewById(R.id.button2) as Button
         btnClear.isEnabled = true
     }
 
     fun clear(view: View) {
-
-        var btnClear = root.findViewById(R.id.button2) as Button
+        val btnClear = root.findViewById(R.id.button2) as Button
         btnClear.isEnabled = false
         val surf = root.findViewById(R.id.imageView) as ImageView
         surf.setImageResource(android.R.color.transparent)
-        var btn = root.findViewById(R.id.scrShot) as Button
+        val btn = root.findViewById(R.id.scrShot) as Button
         btn.isEnabled = true
-
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intentMain = intent!!
-
-
         return super.onStartCommand(intent, flags, startId)
     }
 
---
+++ app/src/main/java/com/example/crs/MediaProjectionUtil.kt
@@ -0,0 +1,123 @@
+package com.example.crs
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.PixelFormat
+import android.graphics.Point
+import android.hardware.display.DisplayManager
+import android.media.ImageReader
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.WindowManager
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.Frame
+import com.google.android.gms.vision.text.TextBlock
+import com.google.android.gms.vision.text.TextRecognizer
+
+
+class MediaProjectionUtil {
+    companion object {
+        var mediaProjection: MediaProjection? = null
+        var instance: MediaProjectionUtil? = null
+        var mImageReader: ImageReader? = null
+        var windowSize: Point? = null
+        var appContect: Context? = null
+        var textRecognizer: TextRecognizer? = null
+    }
+
+    fun MediaProjectionUtil(): MediaProjectionUtil? {
+        if (instance == null) {
+            instance = this
+        }
+        return instance
+    }
+
+    fun setupMediaProjection(intentMain: Intent, context: Context){
+        appContect = context.applicationContext
+        val resultCode = intentMain.getIntExtra("resultCode", 0)
+        val data = intentMain.getParcelableExtra<Intent>("mediaProjectionData")
+        val mediaProjectManag =
+            context.getSystemService(AppCompatActivity.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        mediaProjection = mediaProjectManag.getMediaProjection(resultCode, data)
+
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowSize = Point()
+        windowManager.defaultDisplay.getSize(windowSize)
+
+        mImageReader = ImageReader.newInstance(windowSize!!.y, windowSize!!.y, PixelFormat.RGBA_8888, 20)
+context.applicationContext
+        mediaProjection!!.createVirtualDisplay(
+            "ScreenCapture",
+            windowSize!!.x, windowSize!!.y, 1000,
+            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+            mImageReader!!.surface, null, null
+        )
+
+        setupTextParser()
+    }
+
+    fun makeScrShot(): Bitmap {
+
+        var i = 0
+        while (i != 100000) {
+            i++
+            //todo add callback func
+        }
+        val image = mImageReader!!.acquireNextImage()
+        val planes = image!!.planes
+
+        val buffer = planes[0].buffer.rewind()
+        val bitmap = Bitmap.createBitmap(windowSize!!.y, windowSize!!.y, Bitmap.Config.ARGB_8888)
+        bitmap!!.copyPixelsFromBuffer(buffer)
+
+//        return Bitmap.createBitmap(bitmap, 0, 0, windowSize!!.x, windowSize!!.y)
+        val a = Bitmap.createBitmap(bitmap, 0, 0, windowSize!!.x, windowSize!!.y)
+        val b = Bitmap.createBitmap(a,0,300,300,350)
+
+        return Bitmap.createScaledBitmap(b,900,1500,false)
+    }
+
+    fun parseTextFromBitmap(bitmap: Bitmap) {
+
+        val fb = Frame.Builder()
+        fb.setBitmap(bitmap)
+        val frame = fb.build()
+        val parsedTextBlocks = textRecognizer!!.detect(frame)
+        val result = arrayListOf<String>()
+        for (i in 0 until parsedTextBlocks.size()) {
+            try {
+                result.add(parsedTextBlocks[i].value)
+                Log.d("Parser",parsedTextBlocks[i].value)
+            } catch (e: Exception)
+            {}
+        }
+
+    }
+
+    private fun setupTextParser() {
+        textRecognizer = TextRecognizer.Builder(appContect!!).build()
+        textRecognizer!!.setProcessor(object : Detector.Processor<TextBlock> {
+            override fun release() {}
+
+            override fun receiveDetections(detections: Detector.Detections<TextBlock>) {
+                val items = detections.detectedItems
+                val stringBuilder = StringBuffer("")
+
+
+                if (items.size() != 0) {
+
+                    for (i in 0 until items.size()) {
+                        val item = items.valueAt(i)
+                        stringBuilder.append(item.value)
+                        stringBuilder.append("\n")
+                    }
+
+                }
+            }
+        })
+    }
+
+}
