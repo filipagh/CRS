50ca2ae5e279b1bdd9271dc5ec3b9cd91e35584b
Martin Civan <martin.civan5@gmail.com>
2019-08-22 18:57:07 +0200

    bublinka, okienko

--- app/src/main/java/com/example/crs/MainActivity.kt
+++ app/src/main/java/com/example/crs/MainActivity.kt
@@ -5,6 +5,7 @@ import android.media.projection.MediaProjectionManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     lateinit var floatingService: Intent
+    lateinit var setPositionService: Intent
     lateinit var mediaProjectManag: MediaProjectionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         floatingService = Intent(this, FloatingWidgetShowService::class.java)
+        setPositionService = Intent(this, SetPositionService::class.java)
 
         mediaProjectManag = getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
         startActivityForResult(mediaProjectManag.createScreenCaptureIntent(), 100)
@@ -39,26 +42,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
             if (!Settings.canDrawOverlays(this)) {
                 askForOverlayPermission()
-            } else {
-                startService(floatingService)
-
             }
         }
         if (requestCode == 100) {
             floatingService.putExtra("resultCode", resultCode)
             floatingService.putExtra("mediaProjectionData", data)
+            setPositionService.putExtra("resultCode", resultCode)
+            setPositionService.putExtra("mediaProjectionData", data)
 
             // permison na zobrazovanie nad aplikaciami
             if (!Settings.canDrawOverlays(this)) {
                 askForOverlayPermission()
-            } else {
-                startService(floatingService)
             }
         }
 
+    }
+
+    fun start(a: View) {
+        startService(floatingService)
+        moveTaskToBack(true)
+    }
+
+    fun startSetting(a: View) {
+        startService(setPositionService)
+        moveTaskToBack(true)
+    }
+
+    fun end(a: View) {
+        stopService(floatingService)
+        startService(setPositionService)
+        moveTaskToBack(true)
     }
 
 }
--- app/src/main/java/com/example/crs/SetPositionService.kt
+++ app/src/main/java/com/example/crs/SetPositionService.kt
@@ -3,34 +3,28 @@ package com.example.crs
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.graphics.PixelFormat
-import android.media.ImageReader
 import android.os.IBinder
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
-import android.widget.Toast
-import com.example.crs.RoyalApi.PlayerModel
 
-class FloatingWidgetShowService : Service() {
+class SetPositionService : Service() {
 
     lateinit var windowManager: WindowManager
     lateinit var floatingView: View
     lateinit var collapsedView: View
     lateinit var params: WindowManager.LayoutParams
-    lateinit var image: ImageView
 
     override fun onBind(intent: Intent): IBinder? {
-
         return null
     }
 
     override fun onCreate() {
         super.onCreate()
-        floatingView = LayoutInflater.from(this).inflate(R.layout.floating_widget_layout, null)
+        floatingView = LayoutInflater.from(this).inflate(R.layout.set_position_layout, null)
         params = WindowManager.LayoutParams(
             WindowManager.LayoutParams.WRAP_CONTENT,
             WindowManager.LayoutParams.WRAP_CONTENT,
@@ -41,12 +35,10 @@ class FloatingWidgetShowService : Service() {
         windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
         windowManager.addView(floatingView, params)
         collapsedView = floatingView.findViewById(R.id.Layout_Collapsed)
-        image = floatingView.findViewById(R.id.Logo_Icon)
 
-//        collapsedView.setOnClickListener {
-//            expandedView.visibility =  if (expandedView.visibility == View.VISIBLE) View.GONE else View.VISIBLE
-//        }
 
+        floatingView.findViewById<ImageView>(R.id.cancel)
+            .setOnClickListener { stopService(Intent(applicationContext, SetPositionService::class.java)) }
         floatingView.setOnTouchListener(object : View.OnTouchListener {
             var X_Axis: Int = 0
             var Y_Axis: Int = 0
@@ -65,21 +57,9 @@ class FloatingWidgetShowService : Service() {
                         return false
                     }
 
-                    MotionEvent.ACTION_UP -> {
-                        if (shouldClose(params.x, params.y)) {
-                            stopService(Intent(applicationContext, FloatingWidgetShowService::class.java))
-                        }
-                        return false
-                    }
-
                     MotionEvent.ACTION_MOVE -> {
                         params.x = X_Axis + (event.rawX - TouchX).toInt()
                         params.y = Y_Axis + (event.rawY - TouchY).toInt()
-                        if (shouldClose(params.x, params.y)) {
-                            image.setColorFilter(Color.RED)
-                        } else {
-                            image.setColorFilter(Color.TRANSPARENT)
-                        }
                         windowManager.updateViewLayout(floatingView, params)
                         return false
                     }
@@ -88,40 +68,6 @@ class FloatingWidgetShowService : Service() {
             }
         })
     }
-
-    var intentMain: Intent? = null
-    //    var mediaProjection: MediaProjection? = null
-    var mediaProjectionUtil: MediaProjectionUtil? = null
-    lateinit var mImageReader: ImageReader
-
-    fun setup(vstup: View) {
-        if (mediaProjectionUtil == null) {
-            mediaProjectionUtil = MediaProjectionUtil()
-            mediaProjectionUtil!!.setupMediaProjection(intentMain!!, this)
-        }
-    }
-
-    fun scrShot(view: View) {
-//        val btn = root.findViewById(R.id.scrShot) as Button
-//        btn.isEnabled = false
-
-        val scrShot = mediaProjectionUtil!!.makeScrShot()
-        val player = PlayerModel(scrShot)
-
-        Toast.makeText(this, player.playerData?.get("trophies")?.toString(), Toast.LENGTH_LONG).show()
-
-
-//        val surf = root.findViewById(R.id.imageView) as ImageView
-//        surf.setImageBitmap(scrShot)
-
-//        val btnClear = root.findViewById(R.id.button2) as Button
-//        btnClear.isEnabled = true
-    }
-
-    fun shouldClose(x: Int, y: Int): Boolean {
-        return x > -200 && x < 200 && y > 500
-    }
-
 
     override fun onDestroy() {
         super.onDestroy()
